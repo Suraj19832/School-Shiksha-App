45f928c61f325a8fcf9c1c1a1efb278aac961f95
Suraj <suraj.desuntechnology@gmail.com>
2024-05-24 13:05:24 +0530

    banner added

--- src/screens/Deatails.js
+++ src/screens/Deatails.js
@@ -2,52 +2,39 @@ import {
   StyleSheet,
   View,
   Text,
-  TextInput,
   SafeAreaView,
   ScrollView,
-  StatusBar,
   FlatList,
-  Modal,
   TouchableOpacity,
-  Dimensions,
 } from "react-native";
 import React, { useEffect, useRef, useState } from "react";
-import {
-  FontAwesome,
-  Ionicons,
-  MaterialCommunityIcons,
-  MaterialIcons,
-  AntDesign,
-  Entypo,
-  FontAwesome5,
-  Feather,
-  Octicons,
-} from "@expo/vector-icons";
 import { Image } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import Header from "../../components/Header";
 import { useRoute } from "@react-navigation/native";
 import { GetfetchDataWithParams } from "../../Helper/Helper";
-// import { FlatList } from "react-native-web";
 
 const Details = ({ navigation }) => {
   const route = useRoute();
-  const { collegeName, courseName, courseid ,heading,id } = route.params;
-  const [activeIndex, setActiveIndex] = useState(0);
+  const { collegeName, courseName, courseid, heading, id, organization_Id } =
+    route.params;
 
+  const [activeIndex, setActiveIndex] = useState(0);
   const [bannerData, setBannerData] = useState([]);
+
   useEffect(() => {
     const params = {
-      organization_id: courseid,
+      organization_id: organization_Id,
     };
     GetfetchDataWithParams("master/organization-banner", params)
       .then((res) => {
         setBannerData(res.data);
+        console.log(res.data, "Banner Data");
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [organization_Id]);
 
   const images = bannerData;
 
@@ -63,57 +50,41 @@ const Details = ({ navigation }) => {
     return (
       <View style={styles.paginationContainer}>
         <View style={styles.pagination}>
-          {images?.length >0 && images.map((_, index) => (
-            <View
-              key={index}
-              style={[
-                styles.paginationDot,
-                index === activeIndex && styles.paginationDotActive,
-              ]}
-            />
-          ))}
+          {images?.length > 0 &&
+            images.map((_, index) => (
+              <View
+                key={index}
+                style={[
+                  styles.paginationDot,
+                  index === activeIndex && styles.paginationDotActive,
+                ]}
+              />
+            ))}
         </View>
       </View>
     );
   };
 
   const renderItem = ({ item }) => (
-    // console.log(item.banner_image, "helloooooooooo"),
     <View style={styles.imageContainer}>
       <Image
         source={{ uri: item.banner_image }}
         style={styles.image}
         resizeMode="cover"
-        // onError={(error) => console.log("Error loading image:", error)}
       />
     </View>
   );
 
-  // console.log(courseid, "{{{{{{{{{{{{{{{{{{{course id enabs orgauu");
-
-  const [deatailsData, setdeatailsData] = useState([]);
+  const [detailsData, setDetailsData] = useState([]);
   async function fetchUserData(endpoint, id) {
-    // console.log(search_value,"********************************")
     try {
-      // const endpoint = "master/organization-course";
       const params = {
-        // page: 1,
-        // limit: 3,
-        // service_type: serviceType.short_name,
         organization_course_id: id,
-        // course_id:course_idd
       };
 
       GetfetchDataWithParams(endpoint, params).then((res) => {
-        // console.log(
-        //   "free college list api hit status'-____________________",
-        //   res.status
-        // );
-        setdeatailsData(res?.data);
-        // setisLoading(false)
+        setDetailsData(res?.data);
       });
-
-      // console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",userData,params); // Handle or process the fetched user data here
     } catch (error) {
       console.error("Error fetching user data:", error);
     }
@@ -122,7 +93,7 @@ const Details = ({ navigation }) => {
   useEffect(() => {
     fetchUserData("master/course-details", courseid);
   }, [courseid]);
-  // console.log("courseiddd", courseid);
+
   return (
     <SafeAreaView style={styles.container}>
       <Header title={`${heading} LIST`} navigateTo={navigation.goBack} />
@@ -141,8 +112,8 @@ const Details = ({ navigation }) => {
           {renderPagination()}
         </View>
         <View style={styles.listContainer}>
-          {/* cardd */}
-          <View style={styles.listCart}>
+          {/* card */}
+          <View style={styles.listCard}>
             <View style={styles.cardTop}>
               <View
                 style={{
@@ -239,7 +210,7 @@ const Details = ({ navigation }) => {
                   fontSize: 14,
                 }}
               >
-                {deatailsData.course_details}
+                {detailsData.course_details}
               </Text>
             </View>
             <View style={styles.cardButtons}>
@@ -253,7 +224,7 @@ const Details = ({ navigation }) => {
                 }
               >
                 <LinearGradient
-                  colors={["#03357D", "#0569FA"]} // Define your gradient colors here
+                  colors={["#03357D", "#0569FA"]}
                   start={{ x: 0, y: 0.5 }}
                   end={{ x: 1, y: 0.5 }}
                   style={[styles.buttonbox, { justifyContent: "center" }]}
@@ -289,6 +260,7 @@ const Details = ({ navigation }) => {
 };
 
 export default Details;
+
 const styles = StyleSheet.create({
   container: {
     marginBottom: 48,
@@ -326,13 +298,11 @@ const styles = StyleSheet.create({
     height: "auto",
     alignItems: "center",
   },
-  listCart: {
+  listCard: {
     textAlign: "center",
-
     borderWidth: 1,
     borderColor: "white",
     backgroundColor: "white",
-
     borderRadius: 20,
     width: "89%",
     gap: 15,
@@ -343,14 +313,12 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     gap: 10,
-    // padding: 12,
     paddingHorizontal: 20,
     marginTop: 10,
   },
   course: {
     flexDirection: "row",
     gap: 5,
-    // padding: 12,
     paddingHorizontal: 20,
   },
   aboutCourse: {
@@ -360,7 +328,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     gap: 10,
     padding: 12,
-    // position:'absolute'
   },
   cardButtons: {
     paddingHorizontal: 20,
@@ -369,9 +336,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   imgStyle: {
-    // height: Dimensions.get("window").height * 0.29,
-    // width: Dimensions.get("window").width * 0.999,
-    // borderRadius: 10,
     height: 162,
     width: 360,
   },
